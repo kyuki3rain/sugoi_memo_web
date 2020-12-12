edecc5b051cbe6878b395785a9296e49f0efd257
kyuki3rain <kyuki3rain@gmail.com>
2020-12-12 19:13:13 +0900

    electronを削除

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("myAPI", {
-    loadText: () => electron_1.ipcRenderer.invoke("load-text"),
-    readDir: () => electron_1.ipcRenderer.invoke("read-dir"),
-    save: (str) => electron_1.ipcRenderer.invoke("save", str),
-});
-//# sourceMappingURL=preload.js.map
